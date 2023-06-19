329ce599e5a9e8315860f93b90ec4441c6f2a496
Mohsin <mdmohsinhaider6@gmail.com>
2023-06-20 05:17:16 +0530

    init

---
+++ scripit.js
@@ -0,0 +1,17 @@
+let string ="";
+let buttons =document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if (e.target.innerHTML == '=') {
+            string=eval(string)
+        }
+
+        else if (e.target.innerHTML == 'C') {
+            string=""
+        }
+        console.log(e.target)
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+    })
+})
+document.write("this is java scripts");
